92e58118bd5681809682cbe36218dc6aaf441686
Raynos <raynos2@gmail.com>
2013-08-20 18:30:31 -0700

    export zones hash instead

--- all-timezones.js
+++ all-timezones.js
@@ -1,14 +1,3 @@
 var TimezoneData = require("./timezones.json")
 
-var sources = [
-    TimezoneData.links, TimezoneData.meta, TimezoneData.zones
-]
-
-var timezones = {}
-sources.forEach(function (source) {
-    Object.keys(source).forEach(function (key) {
-        timezones[key] = key
-    })
-})
-
-module.exports = timezones
+module.exports = TimezoneData.zones
